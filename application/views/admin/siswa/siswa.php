d33726d41af69755210b1d311e99f77c462c7e8b
Willy kurniawan <willykurniawan50@gmail.com>
2020-11-06 19:23:28 +0700

    tabel pengelolaan siswa

--- application/views/admin/siswa/siswa.php
+++ application/views/admin/siswa/siswa.php
@@ -102,8 +102,8 @@
 
                                     <form action="" method="POST" enctype="multipart/form-data">
                                         <div class="row">
-                                            <div class="col-3">
-                                                <div class="form-inline mx-3 mb-4">
+                                            <div class="col-4">
+                                                <div class="form-inline mx-4 mb-4">
                                                     <label for="filter-siswa">Filter Siswa</label>
                                                     <select class="custom-select ml-2" id="filter-siswa">
                                                         <option value="">Pilih Kelas</option>
@@ -114,115 +114,121 @@
                                                     </select>
                                                 </div>
                                             </div>
-                                            <div class="col-12">
-                                                <table id="dataSiswa" style="width: 100%;" class="table table-striped">
-                                                    <thead class="thead-darkblue">
-                                                        <tr>
-                                                            <th><button type="button" class="btn btn-sm btn-dark" id="checkAll"><i class="fa fa-check" aria-hidden="true"></i></button></th>
-                                                            <th>Nama</th>
-                                                            <th>NIS</th>
-                                                            <th>NISN</th>
-                                                            <th>Kelas</th>
-                                                            <th>Tahun Masuk</th>
-                                                            <th>Tahun Lulus</th>
-                                                            <th>Cash</th>
-                                                        </tr>
-                                                    </thead>
 
-                                                    <tbody>
-                                                        <?php foreach ($siswa as $s) : ?>
-                                                            <tr>
-                                                                <td><input type="checkbox" name="id_siswa" class="id_siswa" value="<?= $s['id'] ?>"></td>
-                                                                <td><?= $s['nama'] ?></td>
-                                                                <td><?= $s['nis'] ?></td>
-                                                                <td><?= $s['nisn'] ?></td>
-                                                                <td><?= $s['kelas'] ?></td>
-                                                                <td><?= $s['tahun_masuk'] ?></td>
-                                                                <td><?= $s['tahun_lulus'] ?></td>
-                                                                <td><?= $s['cash'] ?></td>
-                                                            </tr>
-                                                        <?php endforeach; ?>
-
-                                                    </tbody>
-
-                                                    <tfoot>
-                                                        <tr>
-                                                            <th></th>
-                                                            <th>Nama</th>
-                                                            <th>NIS</th>
-                                                            <th>NISN</th>
-                                                            <th>Kelas</th>
-                                                            <th>Tahun Masuk</th>
-                                                            <th>Tahun Lulus</th>
-                                                            <th>Cash</th>
-                                                        </tr>
-                                                    </tfoot>
-                                                </table>
+                                            <div class="col-3 offset-5">
+                                                <button class="btn btn-success">
+                                                    <i class="fa fa-arrow-up" aria-hidden="true"></i> Naik Kelas
+                                                </button>
                                             </div>
                                         </div>
-                                    </form>
+                                        <div class="col-12">
+                                            <table id="dataSiswa" style="width: 100%;" class="table table-striped">
+                                                <thead class="thead-darkblue">
+                                                    <tr>
+                                                        <th><button type="button" class="btn btn-sm btn-dark" id="checkAll"><i class="fa fa-check" aria-hidden="true"></i></button></th>
+                                                        <th width="15%">Nama</th>
+                                                        <th>NIS</th>
+                                                        <th>NISN</th>
+                                                        <th>Kelas</th>
+                                                        <th>Tahun Masuk</th>
+                                                        <th>Tahun Lulus</th>
+                                                        <th>Cash</th>
+                                                        <th>Action</th>
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    <?php foreach ($siswa as $s) : ?>
+                                                        <tr>
+                                                            <td><input type="checkbox" name="id_siswa" class="id_siswa" value="<?= $s['id'] ?>"></td>
+                                                            <td><?= $s['nama'] ?></td>
+                                                            <td><?= $s['nis'] ?></td>
+                                                            <td><?= $s['nisn'] ?></td>
+                                                            <td><?= $s['kelas'] ?></td>
+                                                            <td><?= $s['tahun_masuk'] ?></td>
+                                                            <td><?= $s['tahun_lulus'] ?></td>
+                                                            <td><?= $s['cash'] ?></td>
+                                                            <td><a href="" class="btn btn-success"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                                                <a href="" class="btn btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                                                            </td>
+                                                        </tr>
+                                                    <?php endforeach; ?>
+
+                                                </tbody>
+                                            </table>
+                                        </div>
                                 </div>
+
+                                </form>
+
+
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- /page content -->
-            <?php $this->load->view('admin/template/footer') ?>
+        </div>
+        <!-- /page content -->
+        <?php $this->load->view('admin/template/footer') ?>
 
-            <script type="text/javascript">
-                var spp = document.getElementById('spp');
-                spp.addEventListener('keyup', function(e) {
-                    // tambahkan 'Rp.' pada saat form di ketik
-                    // gunakan fungsi formatspp() untuk mengubah angka yang di ketik menjadi format angka
-                    spp.value = formatRupiah(this.value);
-                });
+        <script type="text/javascript">
+            var spp = document.getElementById('spp');
+            spp.addEventListener('keyup', function(e) {
+                // tambahkan 'Rp.' pada saat form di ketik
+                // gunakan fungsi formatspp() untuk mengubah angka yang di ketik menjadi format angka
+                spp.value = formatRupiah(this.value);
+            });
 
-                var kat = document.getElementById('kat');
-                kat.addEventListener('keyup', function(e) {
-                    // tambahkan 'Rp.' pada saat form di ketik
-                    // gunakan fungsi formatkat() untuk mengubah angka yang di ketik menjadi format angka
-                    kat.value = formatRupiah(this.value);
-                });
+            var kat = document.getElementById('kat');
+            kat.addEventListener('keyup', function(e) {
+                // tambahkan 'Rp.' pada saat form di ketik
+                // gunakan fungsi formatkat() untuk mengubah angka yang di ketik menjadi format angka
+                kat.value = formatRupiah(this.value);
+            });
 
-                /* Fungsi formatRupiah */
-                function formatRupiah(angka, prefix) {
-                    var number_string = angka.replace(/[^,\d]/g, '').toString(),
-                        split = number_string.split(','),
-                        sisa = split[0].length % 3,
-                        rupiah = split[0].substr(0, sisa),
-                        ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+            /* Fungsi formatRupiah */
+            function formatRupiah(angka, prefix) {
+                var number_string = angka.replace(/[^,\d]/g, '').toString(),
+                    split = number_string.split(','),
+                    sisa = split[0].length % 3,
+                    rupiah = split[0].substr(0, sisa),
+                    ribuan = split[0].substr(sisa).match(/\d{3}/gi);
 
-                    // tambahkan titik jika yang di input sudah menjadi angka ribuan
-                    if (ribuan) {
-                        separator = sisa ? '.' : '';
-                        rupiah += separator + ribuan.join('.');
-                    }
-
-                    rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-                    return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+                // tambahkan titik jika yang di input sudah menjadi angka ribuan
+                if (ribuan) {
+                    separator = sisa ? '.' : '';
+                    rupiah += separator + ribuan.join('.');
                 }
-            </script>
 
-            <script>
-                $(document).ready(function() {
-                    var table = $('#dataSiswa').DataTable();
+                rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+                return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+            }
+        </script>
 
-                    $('#checkAll').on('click', function() {
-                        var siswa_check = $('.id_siswa');
-                        siswa_check.prop('checked', !siswa_check.prop("checked"))
-                    });
-
-                    $('#filter-siswa').on('change', function() {
-                        console.log(this.value)
-                        table.column(4)
-                            .search(this.value)
-                            .draw();
-                    });
-
-
+        <script>
+            $(document).ready(function() {
+                var table = $('#dataSiswa').DataTable({
+                    "lengthMenu": [
+                        [10, 25, 50, -1],
+                        [10, 25, 50, "All"]
+                    ]
                 });
-            </script>
+
+                $('#checkAll').on('click', function() {
+                    var siswa_check = $('.id_siswa');
+                    siswa_check.prop('checked', !siswa_check.prop("checked"))
+                });
+
+                $('#filter-siswa').on('change', function() {
+                    console.log(this.value)
+                    table.column(4)
+                        .search(this.value)
+                        .draw();
+                });
+
+
+            });
+        </script>
 
 
 </body>
